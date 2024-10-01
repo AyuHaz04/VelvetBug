ffa79de6d458c757d28007176791159ddd3ab3bb
Ayush Hazra <ayushh20004@gmail.com>
2024-10-01 09:48:03 +0530

    share buttons added

--- VelvertBug-main/src/pages/CardShow.jsx
+++ VelvertBug-main/src/pages/CardShow.jsx
@@ -17,7 +17,7 @@ export default function CardShow(){
     return(
         <>
         <div className='CardShow'>
-            {/* <h1>Preview</h1> */}
+            
             <div className="containerPreview">
                 <div className="img-container-preview">
                     <img src={url + "/images/" + img} alt="" style={imgStyle3} className='imgplace'/>
@@ -27,6 +27,16 @@ export default function CardShow(){
                         <p>Card Size- 5' x 7'</p>
                         <p><i class="fa-regular fa-image"></i> Download Image</p>
                         <p><i class="fa-solid fa-file-pdf"></i> Download Pdf</p>
+                    </div>
+                    <div className="share-buttons-icons">
+                        {/* <p>Share</p> */}
+                        <FacebookShareButton url={shareUrl}>
+                            <FacebookIcon size={30} round={true} />
+                        </FacebookShareButton>
+                        <WhatsappShareButton url={shareUrl}>
+                            <WhatsappIcon size={30} round={true}/>
+                        </WhatsappShareButton>
+                        
                     </div>
                     <Link to="/CardEdit"><button className='Customize-way'>Customize</button></Link>
                 </div>
